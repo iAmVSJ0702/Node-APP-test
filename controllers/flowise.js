39a18025c92fe600206d511a2bc17c71afeb8a52
iAmVSJ0702 <vsj0702@gmail.com>
2024-12-06 22:16:11 +0530

    first commit

--- controllers/flowise.js
+++ controllers/flowise.js
@@ -8,7 +8,24 @@ export const createPrediction = async (req, res) => {
       question: message,
     };
 
-    res.status(200).json({ message: "Demo Response" });
+    //Call the api
+    const response = await fetch(
+        `${process.env.FLOWISE_URL}/api/v1/prediction/${process.env.FLOW_ID}`,
+        {
+            headers: {
+                Authorization: `Bearer ${process.env.FLOWISE_API_KEY}`,
+                "Content-Type": "application/json"
+            },
+            method: "POST",
+            body: JSON.stringify(flowiseData)
+        }
+    );
+
+    //Setting the data
+    const data = await response.json();
+    console.log(data.text);
+
+   res.status(200).json({ message: data.text });
   } catch (error) {
     console.log(error);
     res.status(500).json({ message: error.message });
